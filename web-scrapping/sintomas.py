64f395bb3e17c6245db51a765b14883f031a04c9
João Hugo Leda <joaohugoleda2@gmail.com>
2024-03-16 23:13:13 -0300

    Add chrome web driver 122

--- web-scrapping/sintomas.py
+++ web-scrapping/sintomas.py
@@ -5,3 +5,12 @@ from selenium.webdriver.chrome.options import Options
 from time import sleep
 
 requests.get('https://www.einstein.br/doencas-sintomas#p=0pi')
+
+options = Options()
+# options.add_argument('--headless')
+options.add_argument('window-size=400,800')
+
+navegador = webdriver.Chrome(options=options)
+
+navegador.get('https://www.einstein.br/doencas-sintomas#p=0pi')
+
